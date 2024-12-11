e6b75c9392448a33e14d5f3108960169a972d033
Daniel Adhitthana <45264548+someguy609@users.noreply.github.com>
2024-12-11 22:48:40 +0700

    fix: key press disabled bug

--- Entities/Enemy.cs
+++ Entities/Enemy.cs
@@ -1,36 +1,5 @@
 namespace dungeon_of_ty;
 
-// public class Enemy : Character
-// {
-// 	private PictureBox _sprite;
-
-// 	public Enemy(Size screenSize) : base("enemy", 100, 100, 100)
-// 	{
-//         _sprite = new PictureBox
-// 		{
-// 			Size = new Size(100, 100),
-// 			SizeMode = PictureBoxSizeMode.StretchImage,
-// 			// Location = new Point(screenSize.Width / 2 - 25, 70),
-// 			Anchor = AnchorStyles.None,
-// 		};
-
-// 		try
-// 		{
-// 			// _sprite.Image = Resource.hands;
-// 			throw new Exception();
-// 		}
-// 		catch (Exception ex)
-// 		{
-// 			_sprite.BackColor = Color.Black;
-// 		}
-//     }
-
-//     public override Control GetRenderedItems()
-//     {
-//         return _sprite;
-//     }
-// }
-
 public enum EnemyState
 {
 	AGGRESSIVE,
@@ -69,6 +38,6 @@ public class Enemy : Character
 
 	public override Control GetRenderedItems()
 	{
-		return new Control(); // TODO
+		return null; // TODO
 	}
 }
--- Entities/Player.cs
+++ Entities/Player.cs
@@ -1,45 +1,15 @@
 namespace dungeon_of_ty;
 
-// public class Player : Character
-// {
-// 	// private PictureBox _hands;
-
-//     public Player(Size screenSize) : base("player", 100, 100, 100)
-// 	{
-// 		// Moves.Add("punch", Moveset.Punch);
-// 		Moves.Add("kick", new Kick());
-// 		Buffs.Add(new Burn());
-
-// 		// _hands = new PictureBox
-// 		// {
-// 		// 	Dock = DockStyle.Bottom,
-// 		// 	Size = new Size(screenSize.Width, 200),
-// 		// 	SizeMode = PictureBoxSizeMode.StretchImage,
-// 		// };
-
-// 		// try
-// 		// {
-// 		// 	// _hands.Image = Resource.hands;
-// 		// 	throw new Exception();
-// 		// }
-// 		// catch (Exception ex)
-// 		// {
-// 		// 	_hands.BackColor = Color.Black;
-// 		// }
-// 	}
-
-//     public override Control GetRenderedItems()
-//     {
-//         return new Control();
-//     }
-// }
-
 public class Player : Character
 {
-	public Player(string name, int health, int attack, int defense, float luck) : base(name, health, attack, defense, luck) {}
+	public Player(string name, int health, int attack, int defense, double luck) : base(name, health, attack, defense, luck) {}
+
+    public void Learn()
+	{
+	}
 
     public override Control GetRenderedItems()
     {
-        return new Control();
+        return null; // TODO
     }
 }
