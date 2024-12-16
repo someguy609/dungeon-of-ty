bc7042447e2d2a5b656b20c16b40ad9c5843ccfe
Gerry Nicholas <gerrynicholas76@gmail.com>
2024-12-17 03:52:52 +0700

    refactor: update lexicon

--- Entities/Vocabulary.cs
+++ Entities/Vocabulary.cs
@@ -8,14 +8,15 @@ public static class Vocabulary
 		"do", "it", "he", "be", "we", "to", "go", "no", 
 		//"so", "up", "us", "as", "at", "an", "in", "on", "of", "if", "is", "it", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it", "is", "if", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it", "is", "if", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it", "is", "if", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it", "is", "if", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it", "is", "if", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it", "is", "if", "by", "my", "me", "am", "an", "as", "at", "to", "do", "go", "so", "no", "on", "in", "up", "us", "we", "he", "be", "it",
 		"nap", "sap", "pat", "hat", "ate", "bat", "cat", 
-		//"sat", "pan", "ran", "tan", "van", "ban", "can", "fan", "may", "ray", "say", "pay", "way", "lay", "day", "ask", "all", "any", "and", "are", "art", "act", "add", "age", "air", "aim", "bag", "bad", "bar", "bat", "bed", "bet", "big", "bit",
+		"sat", "pan", "ran", "tan", "van", 
+		// "ban", "can", "fan", "may", "ray", "say", "pay", "way", "lay", "day", "ask", "all", "any", "and", "are", "art", "act", "add", "age", "air", "aim", "bag", "bad", "bar", "bat", "bed", "bet", "big", "bit",
 		"punch", "kick", "talk", "walk", "slap", "trap", "tomb", 
-		// "word", "same", "very", "tell", "long", 
+		"word", "same", "very", "tell", "long", 
 		"should", "would", "could", "small",  "under", "water", 
-		//"where", "which", "while", "white", "large", 
+		"where", "which", "while", "white", "large", 
 		"aardvark",
 		"serendipity",
-		"pneumonoultramicroscopicsilicovolcanoconiosis"
+		// "pneumonoultramicroscopicsilicovolcanoconiosis"
 	};
 
 	public static string GetWord() { return Words[_random.Next(Words.Count)]; }
